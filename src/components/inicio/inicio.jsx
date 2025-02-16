0f2f80195588c2f4b6c26e0361ae08df06bdaf66
Miguel Aznar <miguelaznar@MacBook-Pro-de-Miguel.local>
2025-02-16 02:36:15 -0300

    Haciendo el inicio

--- src/components/inicio/inicio.jsx
+++ src/components/inicio/inicio.jsx
@@ -1,27 +1,45 @@
+import { useState, useEffect, useRef } from 'react';
 
+const Inicio = () => {
+    const videos = [
+        "/imagenes/abogados-editado1.mp4",
+        "/imagenes/abogados1-1.mp4",
+        "/imagenes/edificios.mp4"
+    ];
+    const [index, setIndex] = useState(0);
+    const [nextIndex, setNextIndex] = useState(1)
+    const [isTransitioning, setIsTransitioning] = useState(false)
+   
 
-
-const Inicio = () =>{
     
-
-
-
-    return(
-        <div>
+    useEffect(() => {
+        const interval = setInterval(() => {
             
-            <div>
-                <video src="/imagenes/abogados.mp4" className="video-inicio"  autoPlay loop muted></video>
-            </div>
+            setIsTransitioning(true)
 
-            <div>
-                <video src="/imagenes/abogados1.mp4" className="video-inicio" autoPlay loop muted></video>
-            </div>
+            setTimeout(()=>{
+                setIndex(nextIndex)
+                setNextIndex((prev) => (prev + 1 ) % videos.length)
+                setIsTransitioning(false)
+            },1000)
+        
+        }, 5000); 
+
+        return () => clearInterval(interval);
+    }, [nextIndex]);
+
+
+
+    return (
+        <div className="video-container">
+            <video  src={videos[index]} key={index} className="video-inicio" autoPlay loop muted></video>
+            <video  src={videos[index]} key={index} className="video-inicio" autoPlay loop muted></video>
+
 
             <h1 className="h3-inicio">Especialistas en Derecho Civil y Real Estate</h1>
             <h3 className="p-inicio">Resolviendo conflictos patrimoniales con eficiencia y profesionalismo</h3>
         </div>
-    )
+    );
 }
 
-
-export default Inicio
+export default Inicio;

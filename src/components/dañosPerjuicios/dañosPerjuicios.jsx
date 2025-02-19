4ca630482f4bc5253026a7b84eabcbcd586820e1
Miguel Aznar <miguelaznar@MacBook-Pro-de-Miguel.local>
2025-02-19 00:39:51 -0300

    haciendo parte daños y perjuicios

--- src/components/dañosPerjuicios/dañosPerjuicios.jsx
+++ src/components/dañosPerjuicios/dañosPerjuicios.jsx
@@ -1,8 +1,14 @@
 const DañosYPerjuicios = ()=>{
     return(
-        <div>
-            <h1>Daños y Perjuicios</h1>
-        </div>
+        <section className="contenedor-daños">
+            <div >
+                <img src="/imagenes/accidente-laboral.jpg" alt="" className="img-daños"/>
+                <img src="/imagenes/accidente-transito.jpg" alt="" className="img-daños"/>
+                <img src="/imagenes/daños-contractuales.jpg" alt="" className="img-daños"/>
+                <img src="/imagenes/mala-praxis.jpg" alt="" className="img-daños"/>
+            </div>
+        </section>
+     
     )
 }
 
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,39 +1,53 @@
 import { Link } from "react-router-dom"
+import ScrollReveal from "../scrollReveal/scrollReveal.jsx"
 
 const Footer = ()=>{
     return(
         <>
         <section className="contenedor-footer">
+            
             <div className="div-footer">
+            <ScrollReveal>
                 <img src="/imagenes/logo-editado.png" className="img-footer" />
                 <div className="div-icons">
                 <i class="bi bi-instagram"></i>
                 <i class="bi bi-facebook"></i>
                 <i class="bi bi-linkedin"></i>
                 </div>
+                </ScrollReveal>
             </div>
 
+
+            
             <div className="div-footer2">
+            <ScrollReveal>
                 <div className="div-footer-contacto">
-                   <h1 className="h3-footer">Contacto</h1> <i class="bi bi-person-rolodex"></i>
+                   <h1 className="h1-footer">Contacto</h1> <i class="bi bi-person-rolodex"></i>
                 </div>
                 <p className="p-footer"><b>Email:</b> blaksleyaznar@gmail.com</p>
                 <p className="p-footer"><b>Telefono:</b> +54 9 11 3215-8091</p>
                 <Link to='/contacto'><button className="boton-footer">CONTACTANOS</button></Link>
+            </ScrollReveal>
             </div>
+            
 
             <div className="div-footer3">
-                <div className="div-footer-contacto">
-                    <h3 className="h3-footer">Oficina</h3> <i className="bi bi-geo-alt-fill"></i>
-                </div>
-                <p className="p-footer"><b>Direccion:</b> Rivadavia 195, <br /> San Isidro | Piso 5 | Depto B</p>
+                <ScrollReveal>
+                    <div className="div-footer-contacto">
+                        <h1 className="h1-footer">Oficina</h1> <i className="bi bi-geo-alt-fill"></i>
+                    </div>
+                        <p className="p-footer"><b>Direccion:</b> Rivadavia 195, <br /> San Isidro | Piso 5 | Depto B</p>
+                        <p>TExto para que </p>
+                        <p>quede bien</p>
+                </ScrollReveal>
             </div>
         </section>
-
-        <hr className="hr-footer"/>
-        <div className="div-hr-footer">
-            <i class="bi bi-c-circle"></i> <p className="p-footer-hr">Blaksley & Aznar | Todos los derechos reservados</p>
-        </div>
+                <ScrollReveal>
+                    <hr className="hr-footer"/>
+                    <div className="div-hr-footer">
+                        <i class="bi bi-c-circle"></i> <p className="p-footer-hr">Blaksley & Aznar | Todos los derechos reservados</p>
+                    </div>
+                </ScrollReveal>
         </>
     )
 }

15b6cf619ae8ce238ffeff242862ad1855bc69e1
Miguel Aznar <miguelaznar@MBP-de-Miguel.fibertel.com.ar>
2025-03-10 22:56:01 -0300

    cache

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePWA from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:'/pagina-web-pitu/'
-})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePWA from '@vitejs/plugin-pwa';
 
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/(.*)\.jpg$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 24 * 60 * 60, // 1 day
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/pagina-web-pitu/',
+});

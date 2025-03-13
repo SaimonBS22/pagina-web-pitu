4bf7113eef7252dbdc48ab7558133bc003eb9966
Miguel Aznar <miguelaznar@MBP-de-Miguel.fibertel.com.ar>
2025-03-12 22:55:12 -0300

    aver

--- src/components/inicio/inicio.jsx
+++ src/components/inicio/inicio.jsx
@@ -12,7 +12,7 @@ const Inicio = () => {
                 <link rel="canonical" href="https://www.blaksley&aznar.com" />
         </Helmet>
         <div className="video-container">
-            <video className="video-inicio" src='https://blaksleyaznar.netlify.app/ed1.mp4' autoPlay muted loop playsInline></video>
+            <video className="video-inicio" src='/assets/ed1.mp4' autoPlay muted loop playsInline></video>
             <h1 className="h3-inicio">Especialistas en Derecho Civil y Real Estate</h1>
             <h3 className="p-inicio">Resolviendo conflictos patrimoniales con eficiencia y profesionalismo</h3>
         </div>
